f1f4005c9c1f88d4d6b16fa53a6e0de35a1b06db
Pavel Fokin <fokinpv@gmail.com>
2019-03-21 22:32:37 +0200

    Add /clips endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 extern crate actix_web;
-use std::env;
+extern crate env_logger;
+#[macro_use]
+extern crate serde_derive;
 
-use actix_web::{server, App, HttpRequest, Responder};
+use std::env;
+use actix_web::{
+    server, http, middleware, App, HttpRequest, HttpResponse, Responder, Json
+};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Item {
+    id: u32,
+    data: String,
+}
 
 // we need to read the PORT from the env variable (Heroku sets it)
 fn get_server_port() -> u16 {
@@ -11,18 +23,29 @@ fn get_server_port() -> u16 {
     .unwrap_or(8181)
 }
 
-fn greet(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+#[allow(unused_variables)]
+fn index(req: &HttpRequest) -> impl Responder {
+    "WebClipper Application"
+}
+
+fn create_clip(item: Json<Item>) -> HttpResponse {
+    println!("model: {:?}", &item);
+    HttpResponse::Ok().json(item.0)
 }
 
 fn main() {
+    ::std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    let port = get_server_port();
+
     server::new(|| {
         App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
+            .middleware(middleware::Logger::default())
+            .resource("/", |r| r.f(index))
+            .resource("/clips", |r| r.method(http::Method::POST).with(create_clip))
     })
-    .bind(format!("0.0.0.0:{}", get_server_port()))
-    .expect("Can not bind to port 8000")
+    .bind(format!("0.0.0.0:{}", port))
+    .expect(&format!("Can not bind to port {}", port))
     .run();
 }
