9841a4c07f1af5023c6d937d499e08ef4afc7611
Pavel Fokin <fokinpv@gmail.com>
2019-03-26 15:37:43 +0200

    Configurate rustfmt for 79 line width

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,10 +3,14 @@ use actix_web::{HttpRequest, HttpResponse, State};
 
 pub struct Clips;
 impl Clips {
-    pub fn get((req, state): (HttpRequest<AppState>, State<AppState>)) -> HttpResponse {
+    pub fn get(
+        (req, state): (HttpRequest<AppState>, State<AppState>),
+    ) -> HttpResponse {
         HttpResponse::Ok().json(state.db.lock().unwrap().all())
     }
-    pub fn get_one((req, state): (HttpRequest<AppState>, State<AppState>)) -> HttpResponse {
+    pub fn get_one(
+        (req, state): (HttpRequest<AppState>, State<AppState>),
+    ) -> HttpResponse {
         let pk: usize = req.match_info().get("id").unwrap().parse().unwrap();
         let item = state.db.lock().unwrap().get(pk);
         HttpResponse::Ok().json(item)
@@ -14,5 +18,12 @@ impl Clips {
     pub fn post((item, state): (Json<Item>, State<AppState>)) -> HttpResponse {
         let created_item = state.db.lock().unwrap().insert(item.clone());
         HttpResponse::Ok().json(created_item)
+    }
+    pub fn delete_one(
+        (req, state): (HttpRequest<AppState>, State<AppState>),
+    ) -> HttpResponse {
+        let pk: usize = req.match_info().get("id").unwrap().parse().unwrap();
+        let item = state.db.lock().unwrap().get(pk);
+        HttpResponse::Ok().json(item)
     }
 }
